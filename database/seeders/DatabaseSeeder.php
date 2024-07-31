55d1b1f28583b3fd822698a660592618f9a73b0a
Auwal Kasim Adam <49225399+Auwalkay@users.noreply.github.com>
2024-07-31 22:23:28 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//         \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 //
-//        $this->call(CategorySeeder::class);
-//        $this->call(ProductSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
 
         $this->call(OrderSeeder::class);
 

5e53698d306a43d87b1bcb026dd61a9bd5cc1f34
auwal adam <kaseemauwal501@gmail.com>
2024-08-02 11:13:40 +0100

    update

--- resources/views/filament/orders/tables/custom-table.blade.php
+++ resources/views/filament/orders/tables/custom-table.blade.php
@@ -70,6 +70,240 @@
 
                 </td>
             </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="w-4 p-4">
+                    <div class="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                    </div>
+                </td>
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">#38944</th>
+                <td class="px-6 py-4">100.09 OAB</td>
+                <td class="px-6 py-4">+234090445</td>
+                <td class="px-6 py-4">Nigeria</td>
+                <td class="px-6 py-4">12345678</td>
+                <td class="px-6 py-4">Abuja</td>
+                <td class="px-6 py-4">27/02/2024</td>
+                <td class="px-6 py-4">
+                    <div class="flex item-center">
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-eye" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                        <a href="#" class="font-medium text-gray-600 dark:text-blue-500 hover:underline">
+                            <x-filament::icon icon="heroicon-o-trash" class="w-5 h-5  mx-2"></x-filament::icon>
+                        </a>
+                    </div>
+
+                </td>
+            </tr>
 
             {{--        @endforeach--}}
 
